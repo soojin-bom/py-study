97ba2c75c0da6ae4945fbf430ca714d95aa5437e
Soojin Lee <soojinleeoncloud@gmail.com>
2020-03-14 12:57:45 +0900

    making heart by using Python
    -거북이로 하트 그리기
    -반복문으로 반원 그리기
    -실패 but 재미를 얻음

---
+++ first-heart-200314.py
@@ -0,0 +1,39 @@
+Python 3.8.2 (v3.8.2:7b3ab5921f, Feb 24 2020, 17:52:18) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle as t
+>>> for x in range(180):
+	t.forward(1)
+	t.left(1)
+
+	
+>>> t.left(45)
+>>> t.forward(30)
+>>> t.forward(100)
+>>> t.forward(20)
+>>> t.forward(10)
+>>> t.left(90)
+>>> t.forward(150)
+>>> t.fillcolor("red")
+>>> t.begin_fill()
+>>> t.left(90)
+>>> for x turtle as t
+SyntaxError: invalid syntax
+>>> for x in range(180):
+	t.forward(1)
+	t.left(1)
+
+	
+>>> t.left(90)
+>>> t.forward(50)
+>>> t.forward(60)
+>>> t.forward(10)
+>>> t.left(90)
+>>> t.right(45)
+>>> for x in range(180):
+	t.forward(1)
+	t.left(1)
+
+	
+>>> t.end_fill()
+>>> 
---
+++ second-heart-200314.py
@@ -0,0 +1,31 @@
+Python 3.8.2 (v3.8.2:7b3ab5921f, Feb 24 2020, 17:52:18) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle as t
+>>> t.color("red")
+>>> t.left(90)
+>>> t.right(45)
+>>> t.forward(100)
+>>> t.left(45)
+>>> for x in range(180):
+	t.forward(1)
+	t.left(1)
+
+	
+>>> t.left(180)
+>>> for x in range(180):
+	t.forward(1)
+	t.left(1)
+
+	
+>>> t.left(45)
+>>> t.forward(100)
+>>> t.begin_fill()
+>>> t.forward(50)
+>>> t.left(90)
+>>> t.forward(50)
+>>> t.right(45)
+>>> t.forward(10)
+>>> t.forward(10)
+>>> t.end_fill()
+>>> 
