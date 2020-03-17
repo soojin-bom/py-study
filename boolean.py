5588f5c61e713b967f3953ba2bc1c9661eb4f1aa
Soojin Lee <soojinleeoncloud@gmail.com>
2020-03-17 23:28:06 +0900

    Create boolean.py
    -Boolean 학습
    -좌항, 우항 비교 학습
    -os.path 학습

---
+++ boolean.py
@@ -0,0 +1,27 @@
+#Number. Integer
+print(1)
+
+#String
+print("Hello World!")
+
+#Boolean
+print(True)
+print(False)
+
+#Expression
+print(1+1)
+print("Hello "+"World!")
+
+#Comparison Operator
+print(1==1)
+print(1<2)
+print(1>2)
+
+#Membership Operator
+print("Is This True?")
+print("m"in"mummu")
+
+#Check files in Directory
+print("Check files in Directory")
+import os.path
+print(os.path.exists('boolean.py'))
