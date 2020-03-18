7020f9a72c8191f570bf5fe3248d296ded024169
Soojin Lee <soojinleeoncloud@gmail.com>
2020-03-18 23:30:28 +0900

    Create conditional.py
    -변수에 input값 할당하는 법 학습
    -if문 학습
    -if문은 중첩될 수 있다.

---
+++ conditional.py
@@ -0,0 +1,13 @@
+#그 후, 변수 user_input 을 출력하면 input 값이 출력된다.
+#print(user_input)
+
+# 변수 user_input 에 input 값을 대입한다. 
+user_id = input('id?')
+user_pwd = input('password?')
+if user_id == 'soojin':
+    if user_input == 'sjsj':
+        print('hello, master.')
+    else:
+        print("who are you?")
+else:
+    print("who are you?")
