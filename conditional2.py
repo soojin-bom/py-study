ed07bd0394faf20df0de420c88efaa25a2528bf1
Soojin Lee <soojinleeoncloud@gmail.com>
2020-03-19 22:42:43 +0900

    Create conditional2.py
    -elif
    -and, or, not
    -userA 말고, userB도 로그인할 수 있도록

---
+++ conditional2.py
@@ -0,0 +1,9 @@
+user_id = input('id?')
+user_pwd = input('password?')
+
+if user_id == 'soojin' and user_pwd == 'sjsj':
+    print("Hello, Soojin")
+elif user_id =='mummu' and user_pwd == "mmmm":
+    print("Hello, mummu")
+else:
+    print("Who are you?")
