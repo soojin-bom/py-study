75b1210bdafdbce45f24b63db596daa20e24176e
Soojin Lee <soojinleeoncloud@gmail.com>
2020-03-24 18:25:30 +0900

    Update index.py
    -반복문을 이용해 글목록 구현

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
 #!/usr/local/bin/python3
 print("Content-Type: text/html")
 print()
-import cgi
+import cgi, os
+
+files = os.listdir('data')
+print(files)
+listStr = ''
+for item in files:
+      listStr = listStr + '<li><a href = "index.py?id={name}">{name}</a></li>'.format(name=item)
+
 form = cgi.FieldStorage()
-pageId = form["id"].value
+if 'id' in form:
+      pageId = form["id"].value
+      description = open('data/'+pageId, 'r').read()
+else:
+      pageId = 'Welcome'
+      description = 'Hellow, web'
 print('''<!doctype html>
 <html>
 <head>
@@ -13,12 +25,10 @@ print('''<!doctype html>
 <body>
   <h1><a href="index.py?id=WEB">WEB</a></h1>
   <ol>
-    <li><a href="index.py?id=HTML">HTML</a></li>
-    <li><a href="index.py?id=CSS">CSS</a></li>
-    <li><a href="index.py?id=JavaScript">JavaScript</a></li>
+    {listStr}
   </ol>
   <h2>{title}</h2>
-  <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
+  <p>{desc}
   </p>
 </body>
-</html>'''.format(title=pageId))
+</html>'''.format(title=pageId, desc=description, listStr=listStr))
