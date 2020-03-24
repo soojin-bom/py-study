b3e015ff55d6c5370592307d43784d5101956033
Soojin Lee <soojinleeoncloud@gmail.com>
2020-03-24 19:07:55 +0900

    form 생성

---
+++ create.py
@@ -0,0 +1,39 @@
+#!/usr/local/bin/python3
+print("Content-Type: text/html")
+print()
+import cgi, os
+
+files = os.listdir('data')
+print(files)
+listStr = ''
+for item in files:
+      listStr = listStr + '<li><a href = "index.py?id={name}">{name}</a></li>'.format(name=item)
+
+form = cgi.FieldStorage()
+if 'id' in form:
+      pageId = form["id"].value
+      description = open('data/'+pageId, 'r').read()
+else:
+      pageId = 'Welcome'
+      description = 'Hellow, web'
+print('''<!doctype html>
+<html>
+<head>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.py?id=WEB">WEB</a></h1>
+  <ol>
+    {listStr}
+  </ol>
+  <a href="create.py">create</a>
+  <h2>{title}</h2>
+  <form action="process_create.py method="post">
+    <p><input type="text" name="title"placeholder="title">
+    </p>
+    <p><textarea rows="4" name="description"placeholder="description"></textarea></p>
+    <p><input type="submit"></p>
+    </form>
+</body>
+</html>'''.format(title=pageId, desc=description, listStr=listStr))
