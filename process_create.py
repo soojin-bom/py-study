04c33b5b892e2d573ff630bb5e72c99f1c744e5a
Soojin Lee <soojinleeoncloud@gmail.com>
2020-03-26 22:47:40 +0900

    Update process_create.py
    -form 제출한 내용을 바탕으로 파일 생성

---
+++ process_create.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+
+
+import cgi
+form = cgi.FieldStorage()
+
+title = form["title"].value
+description = form['description'].value
+
+opened_file = open('data/'+title, 'w')
+opened_file.write(description)
+
+#Redirection
+print("Location: index.py?id="+title)
+print()
