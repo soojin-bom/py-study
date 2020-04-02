fec2de4fcb51ea9a5f55bd606e1cf033cbd6e6fc
Soojin Lee <soojinleeoncloud@gmail.com>
2020-04-02 10:23:28 +0900

    Update func.py
    -parameter 학습
    -argument 학습
    -함수 1개당 1개의 기능만 하는 것이 좋다.

--- func.py
+++ func.py
@@ -5,6 +5,7 @@ s=a+b+c
 r=s/3
 print(r)
 
+'''
 def average():
     a=1
     b=2
@@ -13,3 +14,28 @@ def average():
     r=s/3
     print(r)
 average()
+'''
+
+'''
+#input에 관한 함수
+#a,b,c는 매개변수. parameter
+def average(a,b,c):
+    s=a+b+c
+    r=s/3
+    print(r)
+
+#10, 20, 30은 인자, 즉 argument
+average(10,20,30)
+'''
+
+#하나의 함수에는 하나의 기능만 갖고있는 것이 좋다.
+def average(a,b,c):
+    s=a+b+c
+    r=s/3
+    return r
+
+print(average(10,20,30))
+
+def a():
+    return 'haha'
+print(a())
