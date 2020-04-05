f186b1b809ebb15d1b3c780b2335d96ad54a7e42
Soojin Lee <soojinleeoncloud@gmail.com>
2020-04-05 16:54:32 +0900

    import module

---
+++ module.py
@@ -0,0 +1,13 @@
+'''
+import math
+
+print(math.average(1,2,3))
+print(math.plus(10,20))
+print(math.pi)
+'''
+
+from math import average, plus, pi
+
+print(average(1,2,3))
+print(plus(10,20))
+print(pi)
